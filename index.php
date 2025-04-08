f01f524c74d8b71e8602e3d958aeb17570dea633
GiriPurushotam <giripurushotam39@gmail.com>
2025-04-08 22:08:11 +0545

    Added navbar to the app

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo "Discussion app";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Discussion App</title>
+    <?php include('./client/commonFiles.php') ?>
+</head>
+
+<body>
+    <?php include('./client/header.php'); ?>
+</body>
+
+</html>
