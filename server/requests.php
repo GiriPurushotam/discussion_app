969ab0a27d954fb3350b357ca621f32b113d0192
GiriPurushotam <giripurushotam39@gmail.com>
2025-04-12 22:10:38 +0545

    Added session for user

--- server/requests.php
+++ server/requests.php
@@ -1,8 +1,21 @@
 <?php
+session_start();
 include('../common/db.php');
 if (isset($_POST['signup'])) {
-    echo "my name is " . $_POST['username'] . '</br>';
-    echo "my email is " . $_POST['email'] . '</br>';
-    echo "my password is " . $_POST['password'] . '</br>';
-    echo "my address is " . $_POST['address'] . '</br>';
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $address = $_POST['address'];
+
+    $users = $conn->prepare("INSERT INTO users (username, email, password, address) VALUES (?, ?, ?, ?)");
+    $users->bind_param("ssss", $username, $email, $password, $address);
+
+    $result = $users->execute();
+
+    if ($result) {
+        echo 'Data Inserted Sucessfully';
+        $_SESSION["user"] = ["username" => $username, "email" => $email];
+    } else {
+        echo 'Error Data Inserting';
+    }
 }
